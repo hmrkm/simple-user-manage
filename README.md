58501aeef33aa2aa5708f29b3104bc1be84b20fc
hmrkm <h.murakami@m-gild.com>
2021-09-22 22:42:39 +0900

    Update Readme

--- README.md
+++ README.md
@@ -12,7 +12,13 @@
 
 1. `.env.sample`をコピーして`.env`を作成
 2. `.env`の内容を修正
-3. `docker-compose up -d`
-4. DBに`app/docs/migration.sql`の内容を反映
+3. `auth/.env.sample`をコピーして`auth/.env`を作成
+4. `auth/.env`の内容を修正
+5. `docker-compose up -d`
+6. DBに`app/docs/migration.sql`の内容を反映
+7. `app/docs/insert_test_user.sql`でテストデータを追加
 
 ## 使い方
+
+1. `http://localhsot:8080/v1/auth`で認証し、トークンを取得
+2. `auth_token`ヘッダーに取得したトークンを書き込むことで各APIが叩けるようになる
---
+++ app/docs/migration.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: mysql
+-- 生成日時: 2021 年 8 月 13 日 07:45
+-- サーバのバージョン： 5.7.35
+-- PHP のバージョン: 7.4.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `tokens`
+--
+
+CREATE TABLE `tokens` (
+  `token` varchar(255) NOT NULL COMMENT 'トークン',
+  `user_id` varchar(255) NOT NULL COMMENT 'users.id',
+  `expired_at` timestamp NULL DEFAULT NULL COMMENT '有効期限',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `users`
+--
+
+CREATE TABLE `users` (
+  `id` varchar(255) NOT NULL COMMENT '主キー',
+  `email` varchar(255) NOT NULL COMMENT 'メールアドレス',
+  `password` varchar(255) NOT NULL COMMENT 'ハッシュ化されたパスワード',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

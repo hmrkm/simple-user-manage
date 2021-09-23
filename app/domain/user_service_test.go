f7e0a02316c45a8cb0c2285680e47464f2785995
hmrkm <h.murakami@m-gild.com>
2021-09-23 19:25:15 +0900

    Update user_service_test.go

--- app/domain/user_service_test.go
+++ app/domain/user_service_test.go
@@ -151,6 +151,42 @@ func TestReadList(t *testing.T) {
 			nil,
 		},
 		{
+			"limitマイナス正常ケース",
+			1,
+			-1,
+			0,
+			"aaa",
+			"example@mail.com",
+			"bbb",
+			nil,
+			[]User{
+				{
+					Id:             "aaa",
+					Email:          "example@mail.com",
+					HashedPassword: "bbb",
+				},
+			},
+			nil,
+		},
+		{
+			"pageマイナス正常ケース",
+			-1,
+			10,
+			0,
+			"aaa",
+			"example@mail.com",
+			"bbb",
+			nil,
+			[]User{
+				{
+					Id:             "aaa",
+					Email:          "example@mail.com",
+					HashedPassword: "bbb",
+				},
+			},
+			nil,
+		},
+		{
 			"異常ケース",
 			1,
 			10,
@@ -189,10 +225,100 @@ func TestReadList(t *testing.T) {
 			actual, actualErr := us.ReadList(tc.page, tc.limit)
 
 			if diff := cmp.Diff(tc.expected, actual); diff != "" {
-				t.Errorf("Create() value is missmatch :%s", diff)
+				t.Errorf("ReadList() value is missmatch :%s", diff)
 			}
 			if !errors.Is(actualErr, tc.expectedErr) {
-				t.Errorf("Verify() actualErr: %v, ecpectedErr: %v", actualErr, tc.expectedErr)
+				t.Errorf("ReadList() actualErr: %v, ecpectedErr: %v", actualErr, tc.expectedErr)
+			}
+		})
+	}
+}
+
+func TestCount(t *testing.T) {
+	testCases := []struct {
+		name        string
+		dbCount     int64
+		dbErr       error
+		expected    int
+		expectedErr error
+	}{
+		{
+			"正常ケース",
+			10,
+			nil,
+			10,
+			nil,
+		},
+		{
+			"異常ケース",
+			10,
+			ErrDBAccess,
+			0,
+			ErrDBAccess,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			sm := NewMockStore(ctrl)
+			sm.EXPECT().Count(gomock.Any(), gomock.Any()).DoAndReturn(
+				func(target *User, count *int64) error {
+					if tc.dbErr == nil {
+						*count = tc.dbCount
+					}
+					return tc.dbErr
+				},
+			)
+			us := NewUserService(sm)
+
+			actual, actualErr := us.Count()
+
+			if diff := cmp.Diff(tc.expected, actual); diff != "" {
+				t.Errorf("Count() value is missmatch :%s", diff)
+			}
+			if !errors.Is(actualErr, tc.expectedErr) {
+				t.Errorf("Count() actualErr: %v, ecpectedErr: %v", actualErr, tc.expectedErr)
+			}
+		})
+	}
+}
+
+func TestVerifyPassword(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    string
+		confirm  string
+		expected bool
+	}{
+		{
+			"不一致ケース",
+			"aaa",
+			"bbb",
+			false,
+		},
+		{
+			"一致ケース",
+			"aaa",
+			"aaa",
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			sm := NewMockStore(ctrl)
+			us := NewUserService(sm)
+
+			actual := us.VerifyPassword(tc.input, tc.confirm)
+
+			if diff := cmp.Diff(tc.expected, actual); diff != "" {
+				t.Errorf("VerifyPassword() value is missmatch :%s", diff)
 			}
 		})
 	}
